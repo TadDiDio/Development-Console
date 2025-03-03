ee5f218a83e443f084aaa9a8b7ae1dc67dc49242
TadDiDio <taddidio@vt.edu>
2025-03-03 14:18:34 -0500

    Fixed event system bug

--- Assets/DeveloperConsole/Scripts/EventSystemManager.cs
+++ Assets/DeveloperConsole/Scripts/EventSystemManager.cs
@@ -3,38 +3,21 @@ using Unity.VisualScripting;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.InputSystem;
 
 namespace DeveloperConsole
 {
     public class EventSystemManager : MonoBehaviour
     {
-        [SerializeField] private InputActionAsset developerConsoleActionMap;
-
         private EventSystem eventSystem;
 
         private void Awake()
         {
             eventSystem = GetComponent<EventSystem>();
 
-            #if ENABLE_INPUT_SYSTEM
-            InputSystemUIInputModule module = eventSystem.AddComponent<InputSystemUIInputModule>();
-            module.actionsAsset = developerConsoleActionMap;
-            #else
+            #if !ENABLE_INPUT_SYSTEM
             eventSystem.AddComponent<StandaloneInputModule>();
+            Destroy(eventSystem.GetComponent<InputSystemUIInputModule>());
             #endif
-        }
-
-        /// <summary>
-        /// Sets the input map.
-        /// </summary>
-        /// <param name="input">The map to set to.</param>
-        public void SetInputMap(DeveloperConsoleInput input)
-        {
-            if (eventSystem.TryGetComponent(out InputSystemUIInputModule module))
-            {
-                //module.actionsAsset = input;
-            }
         }
         private void OnEnable()
         {
